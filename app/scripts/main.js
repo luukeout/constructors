04427502e5bc742644f31050409d875a93fd041d
luke olson <luukeout@gmail.com>
2014-05-19 21:27:06 -0400

    steve's fuel and strength decrease

---
+++ app/scripts/main.js
@@ -0,0 +1,38 @@
+
+// Defining the constructor function called Hero. 
+// The capital H indicates this is a constructor.
+// "this" refers to the instantiated object which is, in this
+// case steve.
+
+function Hero () {
+
+	this.strength = 10;
+	
+	this.fuel = 10;
+	this.swingAt = function(target){
+		target.strength = target.strength - 1,
+		
+		this.fuel = this.fuel -1
+
+	if (this.fuel < 8) {
+		this.strength = this.strength -2
+	} 
+		
+	}
+}
+
+
+function Enemy () {
+	this.strength = 10;
+	// this.speed = 10;
+	this.fuel = 10;
+	this.hit = function(Enemy){
+		this.strength = this.strength + 1
+	};
+};
+
+steve = new Hero();
+frank = new Enemy();
+
+// steve.hit(frank)
+// console.log(frank);
